d12864f8c51a8740df1f6aa1971c2242ba31d221
Sean Ryan Rivera <srivera8@myseneca.ca>
2024-11-16 20:12:29 -0500

    first functions finished

--- assignment1.py
+++ assignment1.py
@@ -4,16 +4,16 @@
 OPS445 Assignment 1
 Program: assignment1.py 
 The python code in this file is original work written by
-"Student Name". No code in this file is copied from any other source
+Sean Ryan Rivera. No code in this file is copied from any other source
 except those provided by the course instructor, including any person,
 textbook, or on-line resource. I have not shared this python script
 with anyone or anything except for submission for grading. I understand
 that the Academic Honesty Policy will be enforced and
 violators will be reported and appropriate action will be taken.
 
-Author: <Student Name>
-Semester: <Fall/Winter/Summer> <Year>
-Description: <fill this in>
+Author: Sean Ryan Rivera
+Semester: Fall 2024
+Description: This program adds or subtracts from a date given by the user.
 '''
 
 import sys
@@ -30,11 +30,31 @@ def day_of_week(date: str) -> str:
 
 def leap_year(year: int) -> bool:
     "return true if the year is a leap year"
-    ...
+    leap_flag = False
+    
+    lyear = year % 4
+    if lyear == 0:
+        leap_flag = True
+    
+    lyear = year % 100
+    if lyear == 0:
+        leap_flag = False
+        
+    lyear = year % 400
+    if lyear == 0:
+        leap_flag = True
+        
+    return leap_flag
 
 def mon_max(month:int, year:int) -> int:
     "returns the maximum day for a given month. Includes leap year check"
-    ...
+    mon_dict = {1:31, 2:28, 3:31, 4:30, 5:31, 6:30,
+                7:31, 8:31, 9:30, 10:31, 11:30, 12:31}
+    
+    if month == 2 and leap_year(year):
+        return 29
+    else:
+        return mon_dict[month]
 
 def after(date: str) -> str: 
     '''
@@ -77,7 +97,40 @@ def after(date: str) -> str:
 
 def before(date: str) -> str:
     "Returns previous day's date as DD/MM/YYYY"
-    ...
+    day, mon, year = (int(x) for x in date.split('/'))
+    day = day - 1
+    
+    if day < 1:
+        # Moves to previous month or year
+        mon = mon - 1
+        if mon < 1:
+            mon = 12
+            year = year - 1
+            
+        # Leap year check
+        lyear = year % 4
+        if lyear == 0:
+            leap_flag = True
+        else:
+            leap_flag = False  # this is not a leap year
+        lyear = year % 100
+        if lyear == 0:
+            leap_flag = False  # this is not a leap year
+        lyear = year % 400
+        if lyear == 0:
+            leap_flag = True  # this is a leap year
+            
+        # Retrieve the max days for the month
+        mon_dict = {1:31, 2:28, 3:31, 4:30, 5:31, 6:30,
+                    7:31, 8:31, 9:30, 10:31, 11:30, 12:31}
+        if mon == 2 and leap_flag:
+            mon_max = 29
+        else:
+            mon_max = mon_dict[mon]
+            
+        day = mon_max  # Set to last day of previous month
+        
+    return f"{day:02}/{mon:02}/{year}"
 
 def usage():
     "Print a usage message to the user"
